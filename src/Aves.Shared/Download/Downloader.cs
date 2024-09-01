d5a7c9b9c845ce676270c5e247e51d532434fa8e
litetex <40789489+litetex@users.noreply.github.com>
2024-09-01 19:17:44 +0200

    Fix deprecation

--- src/Aves.Shared/Download/Downloader.cs
+++ src/Aves.Shared/Download/Downloader.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
-using System.Security.Cryptography;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Aves.Shared.Download
@@ -14,16 +13,20 @@ namespace Aves.Shared.Download
       public static void Download(string srcURL, string targetPath)
       {
          Log.Info($"Starting download: '{srcURL}'->'{targetPath}'");
-
          var sw = Stopwatch.StartNew();
 
-         //Webclient overrides existing files
-         using (var webclient = new WebClient())
-            webclient.DownloadFile(srcURL, targetPath);
+         using (var httpClient = new HttpClient())
+            DownloadFileTaskAsync(httpClient, srcURL, targetPath).Wait();
 
          sw.Stop();
          Log.Info($"Downloading from '{srcURL}' took {sw.ElapsedMilliseconds}ms");
       }
 
+      private static async Task DownloadFileTaskAsync(HttpClient client, string uri, string FileName)
+      {
+         using var s = await client.GetStreamAsync(uri);
+         using var fs = new FileStream(FileName, FileMode.Create);
+         await s.CopyToAsync(fs);
+      }
    }
 }
